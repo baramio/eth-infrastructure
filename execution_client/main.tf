00bd431b1bd9c6e4e811dd49ad3326cc4f96c634
Joseph <jyonalee@github.com>
2022-02-04 21:17:08 -0800

    BRMB-31: setting up traefik for tls

--- execution_client/main.tf
+++ execution_client/main.tf
@@ -27,6 +27,22 @@ variable "ec2_name" {
   default     = "2-ec.baramio-nodes.com"
   description = "name of the second node"
 }
+
+variable "cf_email" {
+  default     = "me@baramio-nodes.com"
+  description = "cloudflare email"
+}
+
+variable "cf_token" {
+  default     = "wassup"
+  description = "cloudflare token"
+}
+
+variable "cf_zoneid" {
+  default     = "zone zone zone"
+  description = "cloudflare zone id"
+}
+
 provider "digitalocean" {}
 
 resource "digitalocean_droplet" "execution_client_1" {
@@ -35,7 +51,14 @@ resource "digitalocean_droplet" "execution_client_1" {
   region    = "nyc1"
   size      = "s-2vcpu-4gb"
   tags      = ["geth"]
-  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key, network = var.network })
+  user_data = templatefile("ec_setup.yaml", {
+    ssh_public_key = var.ssh_public_key,
+    network = var.network ,
+    cf_email = var.cf_email,
+    cf_token = var.cf_token,
+    ec_host = "${var.network}-${var.ec1_name}",
+    ecws_host = "${var.network}-ws-${var.ec1_name}"
+  })
 }
 
 resource "digitalocean_droplet" "execution_client_2" {
@@ -44,5 +67,12 @@ resource "digitalocean_droplet" "execution_client_2" {
   region    = "sfo3"
   size      = "s-2vcpu-4gb"
   tags      = ["geth"]
-  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key, network = var.network })
+  user_data = templatefile("ec_setup.yaml", {
+    ssh_public_key = var.ssh_public_key,
+    network = var.network ,
+    cf_email = var.cf_email,
+    cf_token = var.cf_token,
+    ec_host = "${var.network}-${var.ec2_name}",
+    ecws_host = "${var.network}-ws-${var.ec2_name}"
+  })
 }
