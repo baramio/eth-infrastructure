7ed6aff4b11af34646311cce47b73483bc834756
Joseph <jyonalee@github.com>
2022-02-04 20:56:01 -0800

    BRMB-31: setting the network as a parameter, and name of node based on network

--- execution_client/main.tf
+++ execution_client/main.tf
@@ -13,22 +13,36 @@ variable "ssh_public_key" {
   default     = 1
   description = "ssh public key to access droplets"
 }
+variable "network" {
+  default     = "goerli"
+  description = "network the geth node should be running on"
+}
 
+variable "ec1_name" {
+  default     = "1-ec.baramio-nodes.com"
+  description = "name of the first node"
+}
+
+variable "ec2_name" {
+  default     = "2-ec.baramio-nodes.com"
+  description = "name of the second node"
+}
 provider "digitalocean" {}
 
 resource "digitalocean_droplet" "execution_client_1" {
   image     = "ubuntu-20-04-x64"
-  name      = "rinkeby1-ec.baramio-nodes.com"
+  name      = "${var.network}-${var.ec1_name}"
   region    = "nyc1"
   size      = "s-2vcpu-4gb"
   tags      = ["geth"]
-  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key})
+  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key, network = var.network })
 }
 
-#resource "digitalocean_droplet" "execution_client_2" {
-#  image     = "ubuntu-20-04-x64"
-#  name      = "rinkeby2-ec.baramio-nodes.com"
-#  region    = "sfo3"
-#  size      = "s-2vcpu-4gb"
-#  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key})
-#}
+resource "digitalocean_droplet" "execution_client_2" {
+  image     = "ubuntu-20-04-x64"
+  name      = "${var.network}-${var.ec2_name}"
+  region    = "sfo3"
+  size      = "s-2vcpu-4gb"
+  tags      = ["geth"]
+  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key, network = var.network })
+}
--- execution_client/outputs.tf
+++ execution_client/outputs.tf
@@ -1,4 +1,8 @@
-output "ip_address" {
+output "ec_1_ip_addr" {
   value = digitalocean_droplet.execution_client_1.ipv4_address
+  description = "The public IP address of your droplet application."
+}
+output "ec_2_ip_addr" {
+  value = digitalocean_droplet.execution_client_2.ipv4_address
   description = "The public IP address of your droplet application."
 }
