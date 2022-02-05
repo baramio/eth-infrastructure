6636db7ed2c49ad47c29f8eee70f4c3180af913e
Joseph <jyonalee@github.com>
2022-02-04 19:34:28 -0800

    BRMB-31: editing runcmd commands to accommodate for the runs

--- execution_client/main.tf
+++ execution_client/main.tf
@@ -24,10 +24,10 @@ resource "digitalocean_droplet" "execution_client_1" {
   user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key})
 }
 
-resource "digitalocean_droplet" "execution_client_2" {
-  image     = "ubuntu-20-04-x64"
-  name      = "rinkeby2-ec.baramio-nodes.com"
-  region    = "sfo3"
-  size      = "s-2vcpu-4gb"
-  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key})
-}
+#resource "digitalocean_droplet" "execution_client_2" {
+#  image     = "ubuntu-20-04-x64"
+#  name      = "rinkeby2-ec.baramio-nodes.com"
+#  region    = "sfo3"
+#  size      = "s-2vcpu-4gb"
+#  user_data = templatefile("ec_setup.yaml", { ssh_public_key = var.ssh_public_key})
+#}
