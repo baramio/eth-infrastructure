334913d24b95efd93203d0a7894427101b65a689
Joseph <jyonalee@github.com>
2022-02-05 10:09:32 -0800

    BRMB-31: adding ddns_subdomain config and disable terraform cloudflare dns add

--- execution_client/main.tf
+++ execution_client/main.tf
@@ -69,36 +69,36 @@ resource "digitalocean_droplet" "execution_client_1" {
   })
 }
 
-resource "digitalocean_droplet" "execution_client_2" {
-  image     = "ubuntu-20-04-x64"
-  name      = "${var.network}-${var.ec2_name}"
-  region    = "sfo3"
-  size      = "s-2vcpu-4gb"
-  tags      = ["geth"]
-  user_data = templatefile("ec_setup.yaml", {
-    ssh_public_key = var.ssh_public_key,
-    network = var.network ,
-    cf_email = var.cf_email,
-    cf_token = var.cf_token,
-    ec_host = "${var.network}-${var.ec2_name}",
-    ecws_host = "${var.network}-ws-${var.ec2_name}"
-  })
-}
+#resource "digitalocean_droplet" "execution_client_2" {
+#  image     = "ubuntu-20-04-x64"
+#  name      = "${var.network}-${var.ec2_name}"
+#  region    = "sfo3"
+#  size      = "s-2vcpu-4gb"
+#  tags      = ["geth"]
+#  user_data = templatefile("ec_setup.yaml", {
+#    ssh_public_key = var.ssh_public_key,
+#    network = var.network ,
+#    cf_email = var.cf_email,
+#    cf_token = var.cf_token,
+#    ec_host = "${var.network}-${var.ec2_name}",
+#    ecws_host = "${var.network}-ws-${var.ec2_name}"
+#  })
+#}
 
-resource "cloudflare_record" "ec1_cf" {
-  zone_id = var.cf_zoneid
-  name    = "${var.network}-ws-${var.ec1_name}"
-  value   = digitalocean_droplet.execution_client_1.ipv4_address
-  type    = "A"
-  ttl     = 1
-  proxied = true
-}
-
-resource "cloudflare_record" "ec2_cf" {
-  zone_id = var.cf_zoneid
-  name    = "${var.network}-ws-${var.ec2_name}"
-  value   = digitalocean_droplet.execution_client_2.ipv4_address
-  type    = "A"
-  ttl     = 1
-  proxied = true
-}
+#resource "cloudflare_record" "ec1_cf" {
+#  zone_id = var.cf_zoneid
+#  name    = "${var.network}-ws-${var.ec1_name}"
+#  value   = digitalocean_droplet.execution_client_1.ipv4_address
+#  type    = "A"
+#  ttl     = 1
+#  proxied = true
+#}
+#
+#resource "cloudflare_record" "ec2_cf" {
+#  zone_id = var.cf_zoneid
+#  name    = "${var.network}-ws-${var.ec2_name}"
+#  value   = digitalocean_droplet.execution_client_2.ipv4_address
+#  type    = "A"
+#  ttl     = 1
+#  proxied = true
+#}
--- execution_client/outputs.tf
+++ execution_client/outputs.tf
@@ -2,7 +2,7 @@ output "ec_1_ip_addr" {
   value = digitalocean_droplet.execution_client_1.ipv4_address
   description = "The public IP address of your droplet application."
 }
-output "ec_2_ip_addr" {
-  value = digitalocean_droplet.execution_client_2.ipv4_address
-  description = "The public IP address of your droplet application."
-}
+#output "ec_2_ip_addr" {
+#  value = digitalocean_droplet.execution_client_2.ipv4_address
+#  description = "The public IP address of your droplet application."
+#}
