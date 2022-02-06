b0b2705835d95a687f1f98f53381774b958409ef
Joseph <jyonalee@github.com>
2022-02-06 14:11:18 -0800

    BRMB-31: editing the yq commands to properly add entrypoint params

--- execution_client/main.tf
+++ execution_client/main.tf
@@ -74,49 +74,49 @@ resource "digitalocean_droplet" "execution_client_1" {
   })
 }
 
-resource "digitalocean_droplet" "execution_client_2" {
-  image     = "ubuntu-20-04-x64"
-  name      = "${var.network}-${var.ec2_name}"
-  region    = "sfo3"
-  size      = "s-2vcpu-4gb"
-  tags      = ["geth"]
-  user_data = templatefile("ec_setup.yaml", {
-    ssh_public_key = var.ssh_public_key,
-    network = var.network ,
-    cf_email = var.cf_email,
-    cf_token = var.cf_token,
-    ec_host = "${var.network}-${var.ec2_name}",
-    ecws_host = "${var.network}-ws-${var.ec2_name}"
-  })
-}
+#resource "digitalocean_droplet" "execution_client_2" {
+#  image     = "ubuntu-20-04-x64"
+#  name      = "${var.network}-${var.ec2_name}"
+#  region    = "sfo3"
+#  size      = "s-2vcpu-4gb"
+#  tags      = ["geth"]
+#  user_data = templatefile("ec_setup.yaml", {
+#    ssh_public_key = var.ssh_public_key,
+#    network = var.network ,
+#    cf_email = var.cf_email,
+#    cf_token = var.cf_token,
+#    ec_host = "${var.network}-${var.ec2_name}",
+#    ecws_host = "${var.network}-ws-${var.ec2_name}"
+#  })
+#}
 
-resource "cloudflare_load_balancer" "loadbalancer" {
-  zone_id          = "${var.cf_zoneid}"
-  name             = "${var.network}-ws-ec.baramio-nodes.com"
-  fallback_pool_id = cloudflare_load_balancer_pool.pool2.id
-  default_pool_ids = [cloudflare_load_balancer_pool.pool1.id]
-  description      = "load balancer"
-  proxied          = true
-}
-
-resource "cloudflare_load_balancer_pool" "pool1" {
-  name = "${var.network}-lb-pool-1"
-  origins {
-    name    = "${var.network}-ws-ec-1"
-    address = "${var.network}-ws-${var.ec1_name}.baramio-nodes.com"
-    enabled = true
-  }
-  minimum_origins = 1
-  notification_email = "joseph@baramio.com"
-}
-
-resource "cloudflare_load_balancer_pool" "pool2" {
-  name = "${var.network}-lb-pool-2"
-  origins {
-    name    = "${var.network}-ws-ec-2"
-    address = "${var.network}-ws-${var.ec2_name}.baramio-nodes.com"
-    enabled = true
-  }
-  minimum_origins = 1
-  notification_email = "joseph@baramio.com"
-}
+#resource "cloudflare_load_balancer" "loadbalancer" {
+#  zone_id          = "${var.cf_zoneid}"
+#  name             = "${var.network}-ws-ec.baramio-nodes.com"
+#  fallback_pool_id = cloudflare_load_balancer_pool.pool2.id
+#  default_pool_ids = [cloudflare_load_balancer_pool.pool1.id]
+#  description      = "load balancer"
+#  proxied          = true
+#}
+#
+#resource "cloudflare_load_balancer_pool" "pool1" {
+#  name = "${var.network}-lb-pool-1"
+#  origins {
+#    name    = "${var.network}-ws-ec-1"
+#    address = "${var.network}-ws-${var.ec1_name}.baramio-nodes.com"
+#    enabled = true
+#  }
+#  minimum_origins = 1
+#  notification_email = "joseph@baramio.com"
+#}
+#
+#resource "cloudflare_load_balancer_pool" "pool2" {
+#  name = "${var.network}-lb-pool-2"
+#  origins {
+#    name    = "${var.network}-ws-ec-2"
+#    address = "${var.network}-ws-${var.ec2_name}.baramio-nodes.com"
+#    enabled = true
+#  }
+#  minimum_origins = 1
+#  notification_email = "joseph@baramio.com"
+#}
