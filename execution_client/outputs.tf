65cdda3c7bc6ae7752e03db0c6d9bbce6afde0a9
Joseph <jyonalee@github.com>
2022-02-10 13:31:43 -0800

    BRMB-31: test setup to expose the tunnel URL to test the wss endpoint first

--- execution_client/outputs.tf
+++ execution_client/outputs.tf
@@ -3,7 +3,7 @@ output "ec_1_ip_addr" {
   description = "The public IP address of your droplet application."
 }
 output "tunnel1" {
-  value = "${cloudflare_argo_tunnel.auto_tunnel.id}.cfargotunnel.com"
+  value = "${cloudflare_argo_tunnel.auto_tunnel1.id}.cfargotunnel.com"
   description = "The URL of the tunnel created for ec1"
 }
 #output "ec_2_ip_addr" {
