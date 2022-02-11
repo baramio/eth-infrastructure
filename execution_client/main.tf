41f5ac95047c963bc80a8b402aa5dbfbc8f162de
Joseph <jyonalee@github.com>
2022-02-11 13:36:50 -0800

    BRMB-31: finalizing the cloudflared tunnel and load balancer setup, turn on droplet monitoring

--- execution_client/main.tf
+++ execution_client/main.tf
@@ -81,86 +81,84 @@ provider "random" {}
 resource "random_id" "tunnel_secret1" {
   byte_length = 35
 }
-
+# The random_id resource is used to generate a 35 character secret for the tunnel
+resource "random_id" "tunnel_secret2" {
+  byte_length = 35
+}
 # A Named Tunnel resource called zero_trust_ssh_http
 resource "cloudflare_argo_tunnel" "auto_tunnel1" {
   account_id = var.cf_acctid
   name       = "${var.network}-${var.ec1_name}-tunnel"
   secret     = random_id.tunnel_secret1.b64_std
 }
-
-# DNS settings to CNAME to tunnel target for HTTP application
-resource "cloudflare_record" "eth1-output" {
-  zone_id = var.cf_zoneid
-  name    = "eth1-wss"
-  value   = "${cloudflare_argo_tunnel.auto_tunnel1.id}.cfargotunnel.com"
-  type    = "CNAME"
-  proxied = true
+# A Named Tunnel resource called zero_trust_ssh_http
+resource "cloudflare_argo_tunnel" "auto_tunnel2" {
+  account_id = var.cf_acctid
+  name       = "${var.network}-${var.ec2_name}-tunnel"
+  secret     = random_id.tunnel_secret1.b64_std
 }
 
 resource "digitalocean_droplet" "execution_client_1" {
-  image     = "ubuntu-20-04-x64"
-  name      = "${var.network}-${var.ec1_name}"
-  region    = "nyc1"
-  size      = "s-2vcpu-4gb"
-  tags      = ["geth"]
-  user_data = templatefile("ec_setup.yaml", {
+  image      = "ubuntu-20-04-x64"
+  name       = "${var.network}-${var.ec1_name}"
+  region     = "nyc1"
+  size       = "s-4vcpu-8gb"
+  tags       = ["geth"]
+  monitoring = true
+  user_data  = templatefile("ec_setup.yaml", {
     ssh_public_key = var.ssh_public_key,
-    network = var.network ,
-    cf_email = var.cf_email,
-    cf_token = var.cf_token,
-    ec_host = "${var.network}-${var.ec1_name}",
-    ecws_host = "${var.network}-ws-${var.ec1_name}",
-    account     = var.cf_acctid,
-    tunnel_id   = cloudflare_argo_tunnel.auto_tunnel1.id,
-    tunnel_name = cloudflare_argo_tunnel.auto_tunnel1.name,
-    secret      = random_id.tunnel_secret1.b64_std
+    network        = var.network,
+    account        = var.cf_acctid,
+    tunnel_id      = cloudflare_argo_tunnel.auto_tunnel1.id,
+    tunnel_name    = cloudflare_argo_tunnel.auto_tunnel1.name,
+    secret         = random_id.tunnel_secret1.b64_std
   })
 }
 
-#resource "digitalocean_droplet" "execution_client_2" {
-#  image     = "ubuntu-20-04-x64"
-#  name      = "${var.network}-${var.ec2_name}"
-#  region    = "sfo3"
-#  size      = "s-2vcpu-4gb"
-#  tags      = ["geth"]
-#  user_data = templatefile("ec_setup.yaml", {
-#    ssh_public_key = var.ssh_public_key,
-#    network = var.network ,
-#    cf_email = var.cf_email,
-#    cf_token = var.cf_token,
-#    ec_host = "${var.network}-${var.ec2_name}",
-#    ecws_host = "${var.network}-ws-${var.ec2_name}"
-#  })
-#}
+resource "digitalocean_droplet" "execution_client_2" {
+  image      = "ubuntu-20-04-x64"
+  name       = "${var.network}-${var.ec2_name}"
+  region     = "nyc1"
+  size       = "s-4vcpu-8gb"
+  tags       = ["geth"]
+  monitoring = true
+  user_data  = templatefile("ec_setup.yaml", {
+    ssh_public_key = var.ssh_public_key,
+    network        = var.network,
+    account        = var.cf_acctid,
+    tunnel_id      = cloudflare_argo_tunnel.auto_tunnel2.id,
+    tunnel_name    = cloudflare_argo_tunnel.auto_tunnel2.name,
+    secret         = random_id.tunnel_secret2.b64_std
+  })
+}
 
-#resource "cloudflare_load_balancer" "loadbalancer" {
-#  zone_id          = "${var.cf_zoneid}"
-#  name             = "${var.network}-ws-ec.baramio-nodes.com"
-#  fallback_pool_id = cloudflare_load_balancer_pool.pool2.id
-#  default_pool_ids = [cloudflare_load_balancer_pool.pool1.id]
-#  description      = "load balancer"
-#  proxied          = true
-#}
-#
-#resource "cloudflare_load_balancer_pool" "pool1" {
-#  name = "${var.network}-lb-pool-1"
-#  origins {
-#    name    = "${var.network}-ws-ec-1"
-#    address = "${var.network}-ws-${var.ec1_name}.baramio-nodes.com"
-#    enabled = true
-#  }
-#  minimum_origins = 1
-#  notification_email = "joseph@baramio.com"
-#}
-#
-#resource "cloudflare_load_balancer_pool" "pool2" {
-#  name = "${var.network}-lb-pool-2"
-#  origins {
-#    name    = "${var.network}-ws-ec-2"
-#    address = "${var.network}-ws-${var.ec2_name}.baramio-nodes.com"
-#    enabled = true
-#  }
-#  minimum_origins = 1
-#  notification_email = "joseph@baramio.com"
-#}
+resource "cloudflare_load_balancer" "loadbalancer" {
+  zone_id          = "${var.cf_zoneid}"
+  name             = "${var.network}-ec.baramio-nodes.com"
+  fallback_pool_id = cloudflare_load_balancer_pool.pool2.id
+  default_pool_ids = [cloudflare_load_balancer_pool.pool1.id]
+  description      = "load balancer"
+  proxied          = true
+}
+
+resource "cloudflare_load_balancer_pool" "pool1" {
+  name = "${var.network}-lb-pool-1"
+  origins {
+    name    = "${var.network}-ec-1"
+    address = "${cloudflare_argo_tunnel.auto_tunnel1.id}.cfargotunnel.com"
+    enabled = true
+  }
+  minimum_origins = 1
+  notification_email = "joseph@baramio.com"
+}
+
+resource "cloudflare_load_balancer_pool" "pool2" {
+  name = "${var.network}-lb-pool-2"
+  origins {
+    name    = "${var.network}-ec-2"
+    address = "${cloudflare_argo_tunnel.auto_tunnel2.id}.cfargotunnel.com"
+    enabled = true
+  }
+  minimum_origins = 1
+  notification_email = "joseph@baramio.com"
+}
