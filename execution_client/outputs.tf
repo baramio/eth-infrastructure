0abf74a92819c0e0e8129d2017d7644523b2a2ed
Joseph <jyonalee@github.com>
2022-02-15 14:31:22 -0800

    BRMB-43: change outputs to private ip since these nodes are now behind a VPC

--- execution_client/outputs.tf
+++ execution_client/outputs.tf
@@ -1,9 +1,9 @@
 output "ec_1_ip_addr" {
-  value = digitalocean_droplet.execution_client_1.ipv4_address
-  description = "The public IP address of your droplet application."
+  value = digitalocean_droplet.execution_client_1.ipv4_address_private
+  description = "The private IP address of your droplet application."
 }
 
 output "ec_2_ip_addr" {
-  value = digitalocean_droplet.execution_client_2.ipv4_address
-  description = "The public IP address of your droplet application."
+  value = digitalocean_droplet.execution_client_2.ipv4_address_private
+  description = "The private IP address of your droplet application."
 }
