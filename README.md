5c995ef7e71b584c0b2de6670295ce5538f305b1
Joseph <jyonalee@github.com>
2022-02-24 21:19:51 -0800

    BRMB-47: deprecate ansible installation in favor of terraform. also deploys to k8s instead of to raw instances

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ ethereum ecosystem-related nodes one-click-deployment repository.
 Current deployments:
 * consensus client
 * execution client
-* ssv
 
 Currently built to deploy onto DigitalOcean only.
 
@@ -13,9 +12,5 @@ Terraform scripts for provisioning and deployment of the execution client.
 High availability, multi-region setup.
 
 ## Consensus Client
-Ansible playbook for provisioning & deployment of the consensus client.
-Currently pulls in [eth-docker](https://github.com/eth-educators/eth-docker) for ease of setup.
-
-## SSV
-Ansible playbook for provisioning & deployment of the [SSV operator node](https://docs.ssv.network/operators/installation-operator-1)
-
+Terraform script for provisioning a consensus client node on Kubernetes.
+TBD: High availability, multi-region setup.
---
+++ consensus_client/main.tf
@@ -0,0 +1,150 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 0.13"
+    }
+  }
+}
+
+variable "network" {}
+
+provider "kubernetes" {
+  config_path    = "baramio-kubeconfig.yaml"
+}
+
+resource "kubernetes_namespace" "consensus_client" {
+  metadata {
+    name = "cc"
+  }
+}
+
+resource "kubernetes_stateful_set" "cc-node" {
+  metadata {
+    name = "cc"
+    namespace = "cc"
+    labels = {
+      app = "cc-node"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "cc-node"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "cc-node"
+        }
+      }
+      spec {
+        container {
+          image = "sigp/lighthouse:latest-modern"
+          name  = "cc-node"
+          port {
+            container_port = 9000
+            name = "peers"
+          }
+          port {
+            container_port = 5052
+            name = "http"
+          }
+          port {
+            container_port = 5054
+            name = "metrics"
+          }
+          args = ["lighthouse", "beacon", "--datadir", "/opt/cc/data", "--http", "--http-address", "0.0.0.0", "--network", var.network, "--metrics", "--metrics-address", "0.0.0.0"]
+          volume_mount {
+            name        = "cc-data"
+            mount_path  = "/opt/cc/data"
+          }
+        }
+      }
+    }
+    volume_claim_template {
+      metadata {
+        name = "cc-data"
+        namespace = "cc"
+      }
+      spec {
+        access_modes = ["ReadWriteOnce"]
+        resources {
+          requests = {
+            storage = "100Gi"
+          }
+        }
+        storage_class_name = "do-block-storage"
+      }
+    }
+    service_name = "cc-node"
+  }
+}
+
+resource "kubernetes_service" "cc_http_service" {
+  metadata {
+    name = "cc-node"
+    namespace = "cc"
+    labels = {
+      app = "cc-node"
+    }
+  }
+  spec {
+    selector = {
+      app = "cc-node"
+    }
+    type = "ClusterIP"
+    port {
+      port = "5052"
+      protocol = "TCP"
+      target_port = "5052"
+      name = "http"
+    }
+    port {
+      port = "5054"
+      protocol = "TCP"
+      target_port = "5054"
+      name = "metrics"
+    }
+  }
+}
+
+resource "kubernetes_service" "cc_peers_service" {
+  metadata {
+    name = "cc-node-peers"
+    namespace = "cc"
+    labels = {
+      app = "cc-node"
+    }
+  }
+  spec {
+    selector = {
+      app = "cc-node"
+    }
+    type = "NodePort"
+    port {
+      port = "9000"
+      protocol = "TCP"
+      target_port = "9000"
+      name = "tcp"
+      node_port = "33900"
+    }
+    port {
+      port = "9000"
+      protocol = "UDP"
+      target_port = "9000"
+      name = "udp"
+      node_port = "33900"
+    }
+  }
+}
